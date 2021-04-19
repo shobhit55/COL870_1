26f2a7e3cfbaa78a636721b0059a4bfbb3a86da0
shobhit55 <34087998+shobhit55@users.noreply.github.com>
2021-04-19 16:28:06 +0530

    s

---
+++ E1_E2/dataset_ner.py
@@ -0,0 +1,72 @@
+# dataset
+def word2ind(word):
+    ind = [word_idx[word] if word in word_idx.keys() else 1] #1
+    for char in word:
+        if char in char_idx.keys():
+            a = char_idx[char]
+        else:
+            a = 1
+        ind.append(a)
+    return ind
+
+class NERGmbData(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.data_file = open(csv_file).read().split('\n\n')[:-1] # deleting the last empty sentence, pd.read_csv(csv_file, sep=" ", header=None, encoding="latin1").dropna()
+        self.root_dir = root_dir
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.data_file)
+    
+    def __getitem__(self, idx): #return words and tags indices
+        sent = self.data_file[idx].split('\n') #list of words with tags
+        sent_ind = [word2ind(w.split(' ')[0]) for w in sent if w!='']
+        sent_tag_ind = [tag_idx[w.split(' ')[3]] for w in sent if w!='']
+        
+        return sent_ind, sent_tag_ind
+
+# https://suzyahyah.github.io/pytorch/2019/07/01/DataLoader-Pad-Pack-Sequence.html
+def pad_collate(batch):
+    (xx, yy) = zip(*batch)
+    max_len = max([len(x) for x in xx])
+    max_word_len = max([max([len(y) for y in x]) for x in xx])
+    xx_pad = torch.zeros(len(xx), max_len, max_word_len+1, dtype=int)
+    yy_pad = -torch.ones(len(xx), max_len, dtype=int)
+    
+    for i, (s,t) in enumerate(zip(xx,yy)):
+        for j, w in enumerate(s):
+            xx_pad[i,j,:len(w)] = torch.tensor(w)
+        yy_pad[i,:len(s)] = torch.tensor(t)
+
+    return xx_pad, yy_pad #, x_lens, y_lens
+
+class NERGmbData_test(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.data_file = open(csv_file).read().split('\n\n')[:-1] # deleting the last empty sentence, pd.read_csv(csv_file, sep=" ", header=None, encoding="latin1").dropna()
+        self.root_dir = root_dir
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.data_file)
+    
+    def __getitem__(self, idx): #return words and tags indices
+        sent = self.data_file[idx].split('\n') #list of words with tags
+        sent_ind = [word2ind(w.split(' ')[0]) for w in sent if w!='']
+        sent_tag_ind = [tag_idx[w.split(' ')[3]] for w in sent if w!='']
+        
+        return sent_ind, sent_tag_ind, idx
+
+def pad_collate_test(batch):
+    (xx, yy, inds) = zip(*batch)
+    lengths = [len(x) for x in xx]
+    max_len = max(lengths)
+    max_word_len = max([max([len(y) for y in x]) for x in xx])
+    xx_pad = torch.zeros(len(xx), max_len, max_word_len+1, dtype=int)
+    yy_pad = -torch.ones(len(xx), max_len, dtype=int)
+    
+    for i, (s,t) in enumerate(zip(xx,yy)):
+        for j, w in enumerate(s):
+            xx_pad[i,j,:len(w)] = torch.tensor(w)
+        yy_pad[i,:len(s)] = torch.tensor(t)
+
+    return xx_pad, yy_pad, lengths, inds #, x_lens, y_lens
--- E1_E2/train_ner.py
+++ E1_E2/train_ner.py
@@ -1,4 +1,46 @@
 --initialization [random | glove ] --char_embeddings [ 0 | 1 ] --layer_normalization [ 0 | 1 ] --crf [ 0 | 1 ]
  --output_file <path to the trained model> --data_dir <directory containing data> 
  --glove_embeddings_file <path to file containing glove embeddings> --vocabulary_output_file <path to the file in which vocabulary will be written>
+glove_file
+pre_tr
+
+# ---------------- inputs ------------------
+cont_train = False
+model_folder = '' #'1.1_pre_tr_dropout'
+save_folder = '' #'1.1_pre_tr_dropout_outputtest'
+
+pre_tr = True #run data header also, if this changed
+norm = False
+crf = True
+char_level = False
+dropout = 0.5
+epochs = 1
+
+batch_size = 128
+no_fine_tune = False
+input_size = 100 +50*char_level
+patience = 4
+lr = 0.001
+optim_key = 'Adam' #'AdaDel' #'SGD', Adam for dropout
+# ----------------------------------
+
+# ----------------- dicts load ---------------------
+tag_idx = {'O':0, 'B-tim':1, 'I-tim':2, 'B-gpe':3, 'I-gpe':4, 'B-eve':5, 'I-eve':6, 'B-per':7, 'I-per':8, 'B-art':9, 'I-art':10, 'B-org':11, 'I-org':12, 'I-geo':13, 'B-geo':14, 'B-nat':15, 'I-nat':16}
+tag_list = ['O', 'B-tim', 'I-tim', 'B-gpe', 'I-gpe', 'B-eve', 'I-eve', 'B-per', 'I-per', 'B-art', 'I-art', 'B-org', 'I-org', 'I-geo', 'B-geo', 'B-nat', 'I-nat']
+num_tags = len(tag_list)
+#char indices
+a_file = open("char_idx_from_train.pkl","rb")
+char_idx = pickle.load(a_file)
+# word_idx and pre_trained tensors
+if pre_tr:
+    pre_trained = torch.load("pre_trained.pt")
+    a_file = open("word_idx_from_glove.pkl","rb")
+    word_idx = pickle.load(a_file) #word indices
+else:
+    pre_trained = None
+    a_file = open("word_idx_from_train.pkl","rb")
+    word_idx = pickle.load(a_file) #word indices
+vocab_size = len(word_idx) 
+char_size = len(char_idx)
+#------------------------------------------------------------------
 
